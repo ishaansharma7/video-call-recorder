e316ec66813406d228c8d7afa29b7fd6fa5bb2cf
Ishaan Sharma <ishaan.sharma@ezeiatech.in>
2022-02-21 14:56:16 +0530

    update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from zoom_recorder import zoom_bot
 from google_meet_recorder import google_bot
 
+
 def start_recording(meeting_link: str, password=''):
     if 'google' in meeting_link:
         google_bot.master(meeting_link)
@@ -9,5 +10,6 @@ def start_recording(meeting_link: str, password=''):
     else:
         print('meeting link not recognised')
 
+
 if __name__ == '__main__':
-    start_recording('https://meet.google.com/gdq-jgvq-qaz')
+    start_recording('https://us04web.zoom.us/j/71716104657?pwd=1qPhtsPlskf07rKTXACVsoJKCOU0g2.1', '9x4VL2')
